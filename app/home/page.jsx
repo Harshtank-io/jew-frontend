0d1195cbb6ff3c297542f865055e3bc2f0cd7796
Harshtank-io <tankharsh67@gmail.com>
2024-12-17 00:03:28 +0530

    first commit

---
+++ app/home/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <>
+      <Button>Hello</Button>
+    </>
+  );
+};
+
+export default HeroSection;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://united-goat-48.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
